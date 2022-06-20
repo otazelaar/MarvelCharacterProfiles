ce5588d8bf9b46f9b354cd1063012c49d1a2d608
owentazelaar <owentazelaar@gmail.com>
2022-06-19 20:33:46 -0500

    Marvel App

---
+++ app/src/main/java/com/otaz/marvelcharacterprofiles/Constants.kt
@@ -0,0 +1,3 @@
+package com.otaz.marvelcharacterprofiles
+
+const val API_BASE_URL = "https://gateway.marvel.com/"
---
+++ app/src/main/java/com/otaz/marvelcharacterprofiles/MainActivity.kt
@@ -0,0 +1,4 @@
+package com.otaz.marvelcharacterprofiles
+
+class MainActivity {
+}
